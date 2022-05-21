86295e4771e498598868650e1439ea7ba852072d
Jonathan Feinberg <jdf@pobox.com>
2022-05-21 13:52:38 -0400

    More geometry.

--- todd/geometry.go
+++ todd/geometry.go
@@ -1,11 +1,18 @@
 package todd
 
+import "math"
+
 // All geometry takes place on a Cartesian plane where X increases to the right
 // and Y increases upward.
 
 // Point is a location in 2D space.
 type Point struct {
 	X, Y float64
+}
+
+// Distance returns the Euclidean distance between two points.
+func Distance(p1, p2 *Point) float64 {
+	return math.Hypot(p1.X-p2.X, p1.Y-p2.Y)
 }
 
 // Rect is an axis-aligned rectangle specified by its bottom left corner and top
@@ -24,13 +31,42 @@ type Rect struct {
 // ⎣ 0      0       1 ⎦
 type Affine [6]float64
 
-// Identity is the identity transformation.
+// Identity is the identity transform.
 func Identity() *Affine {
-	return &Affine{1, 0, 0, 0, 1, 0}
+	return &Affine{
+		1, 0, 0,
+		0, 1, 0,
+	}
 }
 
-// TransformPoint applies the affine transformation to a point,
-// returning a new Point It does not mutate the original point.
+// MakeScale creates a scaling transform.
+func MakeScale(x, y float64) *Affine {
+	return &Affine{
+		x, 0, 0,
+		0, y, 0,
+	}
+}
+
+// MakeTranslate creates a translation transform.
+func MakeTranslate(x, y float64) *Affine {
+	return &Affine{
+		1, 0, x,
+		0, 1, y,
+	}
+}
+
+// MakeRotate creates a rotation transform.
+func MakeRotate(angle float64) *Affine {
+	sin := math.Sin(angle)
+	cos := math.Cos(angle)
+	return &Affine{
+		cos, sin, 0,
+		-sin, cos, 0,
+	}
+}
+
+// TransformPoint applies the affine transform to a Point,
+// returning a new Point. It does not mutate the original point.
 func (m *Affine) TransformPoint(p *Point) *Point {
 	return &Point{
 		m[0]*p.X + m[3]*p.Y + m[2],
--- todd/geometry_test.go
+++ todd/geometry_test.go
@@ -1,12 +1,65 @@
 package todd
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
+
+const epsilon = 1e-6
 
 func TestIdentity(t *testing.T) {
 	m := Identity()
 	for _, p := range []*Point{{0, 0}, {1, 2}, {2, 1}, {0, 0}, {4.1, 1.4}} {
 		if *m.TransformPoint(p) != *p {
 			t.Errorf("Got %v, expected %v", m.TransformPoint(p), p)
+		}
+	}
+}
+
+func TestScale(t *testing.T) {
+	for _, p := range []*Point{{0, 0}, {1, 2}, {2, 1}, {0, 0}, {4.1, 1.4}} {
+		for _, s := range [][2]float64{{0, 0}, {1, 2}, {2, 1}, {-1, -1.5}} {
+			m := MakeScale(s[0], s[1])
+			want := Point{s[0] * p.X, s[1] * p.Y}
+			got := *m.TransformPoint(p)
+			if got != want {
+				t.Errorf("Got %v, want %v", got, want)
+			}
+		}
+	}
+}
+
+func TestTranslate(t *testing.T) {
+	for _, p := range []*Point{{0, 0}, {1, 2}, {2, 1}, {0, 0}, {4.1, 1.4}} {
+		for _, s := range [][2]float64{{0, 0}, {1, 2}, {2, 1}, {-1, -1.5}} {
+			m := MakeTranslate(s[0], s[1])
+			want := Point{s[0] + p.X, s[1] + p.Y}
+			got := *m.TransformPoint(p)
+			if got != want {
+				t.Errorf("Got %v, want %v", got, want)
+			}
+		}
+	}
+}
+
+func TestRotate(t *testing.T) {
+	type tc struct {
+		p     Point
+		angle float64
+		want  Point
+	}
+	for _, tt := range []tc{
+		{Point{0, 0}, 0, Point{0, 0}},
+		{Point{1, 2}, math.Pi / 2, Point{-2, 1}},
+		{Point{2, 1}, math.Pi, Point{-2, -1}},
+		{Point{2, 1}, -math.Pi / 2, Point{1, -2}},
+		{Point{0, 0}, math.Pi / 2, Point{0, 0}},
+		{Point{4.1, 1.4}, math.Pi / 2, Point{-1.4, 4.1}},
+	} {
+		m := MakeRotate(tt.angle)
+		got := *m.TransformPoint(&tt.p)
+		if Distance(&got, &tt.want) > epsilon {
+			t.Errorf("Got %v, want %v when rotating %v by %v", got, tt.want, tt.p, tt.angle)
 		}
 	}
 }
